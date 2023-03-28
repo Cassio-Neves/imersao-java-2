d42e6b0bbe2cf0a8f5e888980efbf70341336cfa
Cassio <cassionevestest2@gmail.com>
2023-03-27 21:00:43 -0300

    desafios

---
+++ Aula 1/Desafios/desafio1/desafio1/src/App.java
@@ -0,0 +1,36 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        // Consumindo a API
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularTVs.json";
+        URI endereco = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+
+        String body = response.body();
+
+        //Pegando os dados
+        jsonParser parser = new jsonParser();
+        List<Map<String, String>> listaSeriesPopulares = parser.parse(body);
+
+        //Exibir os dados
+        for (Map<String,String> seriesPopulares : listaSeriesPopulares) {
+            System.out.printf("TV Series: %s", seriesPopulares.get("fullTitle"));
+            System.out.println();
+            System.out.printf("Poster: %s", seriesPopulares.get("image"));
+            System.out.println();
+            System.out.printf("Rating: %s", seriesPopulares.get("imDbRating"));
+            System.out.println();
+            System.out.println();
+        }
+    }
+}
